9eecc1a4f52db196cfc5f01dafd55f104a15c476
Danny Brown <csengineer13@gmail.com>
2016-12-14 17:45:25 -0500

    Pull in dependent css libraries from /static instead of as resolved dependencies -- best practice outlined in vue-cli docs

--- src/src/App.vue
+++ src/src/App.vue
@@ -43,9 +43,6 @@
 <script>
 // Framework7
 import Framework7 from 'Framework7'
-import Framework7OS from 'Framework7/dist/css/framework7.material.min.css'
-import Framework7Colors from 'Framework7/dist/css/framework7.material.colors.min.css' 
-
 
 export default {
   name: 'app',
